c5720fbe0e2455c5e0f5754dc60420e800ce8802
sxrgi0 <sergiofh.2002@gmail.com>
2025-03-17 19:54:19 +0100

    Changed design

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -9,15 +9,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.slider.Slider
 import kotlin.math.pow
 import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var weightEditText: EditText
-    lateinit var heightEditText: EditText
+
+    lateinit var removeWeightButton: Button
+    lateinit var addWeightButton: Button
+    lateinit var weightTextView: TextView
+
+
+    lateinit var heightSlider: Slider
+    lateinit var heightTextView: TextView
+
+
     lateinit var calculateButton: Button
     lateinit var imcTextView: TextView
+
+
+    var weight = 74.0f
+    var height = 170.0f
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,15 +43,30 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        weightEditText = findViewById(R.id.weightEditText)
-        heightEditText = findViewById(R.id.heightEditText)
+        removeWeightButton = findViewById(R.id.removeWeightButton)
+        addWeightButton = findViewById(R.id.addWeightButton)
+        weightTextView = findViewById(R.id.weightTextView)
+        heightSlider = findViewById(R.id.heightSlider)
+        heightTextView = findViewById(R.id.heightTextView)
         calculateButton = findViewById(R.id.calculateButton)
         imcTextView = findViewById(R.id.imcTextView)
 
-        calculateButton.setOnClickListener {
-            val weight = weightEditText.text.toString().toFloat()
-            val height = heightEditText.text.toString().toFloat()
+        removeWeightButton.setOnClickListener {
+            weight --
+            weightTextView.text = "${weight.toInt()} kg"
+        }
 
+        addWeightButton.setOnClickListener {
+            weight ++
+            weightTextView.text = "${weight.toInt()} kg"
+        }
+
+        heightSlider.addOnChangeListener { slider, value, fromUser ->
+            height = value
+            heightTextView.text = "${value.toInt()} cm"
+        }
+
+        calculateButton.setOnClickListener {
             val imc = weight / (height / 100).pow(2)
             imcTextView.text = String.format("%.2f", imc)
             //imcTextView.text = "$imc"
